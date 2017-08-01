11ae5ce760bb0b09dab49c850398c5636822a5b8
Quentin Read <quentin.read@gmail.com>
2017-07-31 21:10:30 -0400

    Beta-diversity maps and methods Initial commit, docs and figs are not yet complete

--- alldiversity/fia_compilebetapart.r
+++ alldiversity/fia_compilebetapart.r
@@ -3,6 +3,10 @@
 fia_betapart <- list()
 
 radii <- c(1, 5, 7.5, 10, 20, 50, 75, 100, 150, 200, 300) * 1000
+
+### amendment on 31 july: for only the smaller radii right now.
+radii <- radii[1:9]
+
 nslices <- c(rep(20, 9), 250, 250)
 
 for (r in 1:length(radii)) {
@@ -19,6 +23,9 @@ fia_betapart[[r]] <- data.frame(radius = radii[r], do.call('rbind', list_r))
 }
 
 load('/mnt/research/nasabio/data/fia/fiaworkspace2.r')
-fia_betapart <- cbind(fiacoords[rep(1:nrow(fiacoords), each = 20), ], do.call('rbind', fia_betapart))
-write.csv(fia_betapart, file = '/mnt/research/nasabio/data/fia/fia_betapart.csv', row.names = FALSE)
+fia_betapart <- cbind(fiacoords[rep(1:nrow(fiacoords), each = 40), ], do.call('rbind', fia_betapart))
+
+#write.csv(fia_betapart, file = '/mnt/research/nasabio/data/fia/fia_betapart.csv', row.names = FALSE)
+# 31 july version.
+write.csv(fia_betapart, file = '/mnt/research/nasabio/data/fia/fia_betapart_to150.csv', row.names = FALSE)
 
---
+++ figs/fia_div_maps_new.r
@@ -0,0 +1,88 @@
+# Figures with new diversity metrics for FIA plots.
+# Made with both abundance-weighted and non-abundance weighted diversity.
+# Beta diversity is new, while alpha and gamma are both old but now both abundance-weighted and presence-absence are shown.
+
+# Use the newer background types for consistency.
+
+
+# Load data ---------------------------------------------------------------
+
+fp <- 'C:/Users/Q/Dropbox/projects/nasabiodiv'
+fpfig <- 'C:/Users/Q/google_drive/NASABiodiversityWG/Figures/fia_diversity_maps/'
+
+bd <- read.csv(file.path(fp, 'fia_betapart_to150.csv'), stringsAsFactors = FALSE) # Slow.
+ed <- read.csv(file.path(fp, 'fia_elev_stats_noalaska.csv'), stringsAsFactors = FALSE)
+ad <- read.csv(file.path(fp, 'fia_alpha.csv'), stringsAsFactors = FALSE)
+gd <- read.csv(file.path(fp, 'fia_gammadiv.csv'), stringsAsFactors = FALSE)
+
+library(dplyr)
+
+bd <- bd %>%
+  mutate(radius = radius/1000) %>% # put radius in km
+  filter(radius %in% c(5, 10, 20, 50, 100)) %>% # reduce to more manageable size
+  left_join(ed)
+
+ad <- ad %>% left_join(ed)
+gd <- gd %>% left_join(ed)
+
+
+# Methods comparison ------------------------------------------------------
+
+# Before going ahead with maps, we need to compare the methods.
+# Compare Sorensen and Jaccard total beta-diversity.
+# Also, compare Podani and Baselga partitions, with both Sorensen and Jaccard.
+
+library(cowplot)
+library(reshape2)
+
+# Comparison of Sorensen and Jaccard
+
+SJdat <- bd %>% 
+  filter(divtype == 'total', family == 'podani') %>%
+  select(PLT_CN, radius, index, abundance, beta) %>%
+  mutate(abund_name = c('presence-absence', 'abundance-weighted')[abundance+1])
+
+SJdat <- dcast(SJdat, PLT_CN + radius + abund_name ~ index, value.var = 'beta')
+
+ggplot(SJdat, aes(x = jaccard, y = sorensen)) +
+  geom_point() +
+  stat_smooth(method = 'lm') +
+  geom_abline(slope=1, intercept=0, color='red', linetype='dotted') +
+  facet_grid(abund_name ~ radius) +
+  theme_bw() +
+  ggtitle('Comparison of Sorensen and Jaccard indices')
+
+# Comparison of Baselga and Podani decompositions
+
+BPdat <- bd %>%
+  filter(divtype %in% c('replacement_proportion')) %>%
+  select(PLT_CN, radius, family, index, divtype, abundance, beta) %>%
+  mutate(abund_name = c('presence-absence', 'abundance-weighted')[abundance+1])
+
+BPdat_sorensen <- filter(BPdat, index == 'sorensen')
+BPdat_jaccard <- filter(BPdat, index == 'jaccard')
+
+BPdat_sorensen <- dcast(BPdat_sorensen, PLT_CN + radius + abund_name ~ family, value.var = 'beta')
+BPdat_jaccard <- dcast(BPdat_jaccard, PLT_CN + radius + abund_name ~ family, value.var = 'beta')
+
+ggplot(BPdat_sorensen, aes(x = baselga, y = podani)) +
+  geom_point() +
+  stat_smooth(method = 'lm') +
+  geom_abline(slope=1, intercept=0, color='red', linetype='dotted') +
+  facet_grid(abund_name ~ radius) +
+  theme_bw() +
+  ggtitle('Comparison of Baselga and Podani partitioning: proportion due to species replacement', subtitle = 'Sorensen index')
+
+ggplot(BPdat_jaccard, aes(x = baselga, y = podani)) +
+  geom_point() +
+  stat_smooth(method = 'lm') +
+  geom_abline(slope=1, intercept=0, color='red', linetype='dotted') +
+  facet_grid(abund_name ~ radius) +
+  theme_bw() +
+  ggtitle('Comparison of Baselga and Podani partitioning: proportion due to species replacement', subtitle = 'Jaccard index')
+
+# Plotting functions ------------------------------------------------------
+
+# These will plot just some individual maps, rather than the big row of maps formatted for the paper.
+
+
