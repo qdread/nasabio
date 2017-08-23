ea28627b0e132f0804c6b27092b9cf475c797a4a
Quentin Read <quentin.read@gmail.com>
2017-08-23 15:13:34 -0400

    Updates to beta-part code Add phylogenetic diversity Functional is added as well but will not work unless there are more species than traits in all sites. It has also not been tested with >10 sites.

--- betadiversity/beta_part_finalindex.r
+++ betadiversity/beta_part_finalindex.r
@@ -1,12 +1,13 @@
 # Function to run all Baselga's betapart calculations for a given matrix. This will do all pairwise calculations. Hopefully it will be fast.
 # I've decided Baselga x Sorensen x Binary is the best, so that's what we're going to do.
-# Later FD and PD will be incorporated.
+# TD is done now along with PD (binary only). Later FD will be incorporated.
 
 # For fd and pd, requires the tree and the trait matrix in raw form, not distance matrices, unfortunately.
-# It doesn't allow NA and things, so let's stick with taxonomic diversity for now.
+# PD works, but FD has too many limitations. It requires the number of species at each site to always exceed the number of traits used.
+# That is too restrictive so we will have to figure out a better way of doing FD later.
 # Can return pairwise distance matrix as well as the multisite measurement.
 
-beta_part <- function(m, abundance=TRUE, pairwise=FALSE, index_family='sorensen') {
+beta_part <- function(m, abundance=TRUE, pairwise=FALSE, index_family='sorensen', TD=TRUE, FD=FALSE, PD=FALSE, trait_mat = NULL, phylo_tree = NULL) {
   
   require(betapart)
   m_bin <- m
@@ -25,32 +26,66 @@ beta_part <- function(m, abundance=TRUE, pairwise=FALSE, index_family='sorensen'
     beta_abundance <- unlist(beta.multi.abund(core_abundance, index.family = index_family_abund))
   }
   
+  if (FD) {
+    trait_mat <- trait_mat[dimnames(trait_mat)[[1]] %in% dimnames(m_bin)[[2]], ]
+    core_func <- functional.betapart.core(m_bin, traits = as.matrix(trait_mat))
+    beta_func <- unlist(functional.beta.multi(core_func, index.family = index_family))
+  }
+  
+  if (PD) {
+    core_phy <- phylo.betapart.core(m_bin, tree = phylo_tree)
+    beta_phy <- unlist(phylo.beta.multi(core_phy, index.family = index_family))
+  }
+  
   # Calculate pairwise metrics if needed.
   if (pairwise) {
     beta_presence_pair  <- beta.pair(core_presence, index.family = index_family)
     if (abundance) {
       beta_abundance_pair <- beta.pair.abund(core_abundance, index.family = index_family_abund)
     }
+    if (FD) {
+      beta_func_pair <- functional.beta.pair(core_func, traits = trait_mat, index.family = index_family)
+    }
+    if (PD) {
+      beta_phylo_pair <- phylo.beta.pair(core_phy, tree = phylo_tree, index.family = index_family)
+    }
   }
-  # Here we can include functional and phylogenetic if needed.
-  
+
   # Combine and return results.
   res <- data.frame(index = rep(index_family, 3),
-                    divtype = c('replacement','nestedness','total'),
+                    diversity = 'taxonomic',
+                    partition = c('replacement','nestedness','total'),
                     abundance = FALSE,
                     beta = beta_presence)
   if (abundance) {
     res_abund <- data.frame(index = rep(index_family_abund, 3),
-                            divtype = c('replacement','nestedness','total'),
+                            diversity = 'taxonomic',
+                            partition = c('replacement','nestedness','total'),
                             abundance = TRUE,
                             beta = beta_abundance)
     res <- rbind(res, res_abund)
   }
+  if (FD) {
+    res_func <- data.frame(index = rep(index_family, 3),
+                           diversity = 'functional',
+                           partition = c('replacement','nestedness','total'),
+                           abundance = FALSE,
+                           beta = beta_func)
+    res <- rbind(res, res_func)
+  }
+  if (PD) {
+    res_phy <- data.frame(index = rep(index_family, 3),
+                          diversity = 'phylogenetic',
+                          partition = c('replacement','nestedness','total'),
+                          abundance = FALSE,
+                          beta = beta_phy)
+    res <- rbind(res, res_phy)
+  }
   
   if (!pairwise) return(res)
   res <- list(res, beta_presence_pair)
-  if (abundance) {
-    res[[3]] <- beta_abundance_pair
-  }
+  if (abundance) res[[length(res) + 1]] <- beta_abundance_pair
+  if (FD) res[[length(res) + 1]] <- beta_func_pair
+  if (PD) res[[length(res) + 1]] <- beta_phy_pair
   return(res)
 }
--- betadiversity/testbetapart.r
+++ betadiversity/testbetapart.r
@@ -8,6 +8,7 @@ load('/mnt/research/nasabio/data/fia/mats/newmat_100000.r')
 load('/mnt/research/nasabio/data/fia/fiaworkspace2.r') 
 
 traits_imputed <- read.csv('/mnt/research/nasabio/data/fia/traits_imputed_22aug.csv', stringsAsFactors = FALSE, row.names = 1)
+trait_pca <- prcomp(traits_imputed[,c('SLA','SSD','Seed.dry.mass','Plant.lifespan')], scale = TRUE, center = TRUE)
 
 beta_part <- function(m, abundance=TRUE, pairwise=FALSE, index_family='sorensen', TD=TRUE, FD=FALSE, PD=FALSE, trait_mat = NULL, phylo_tree = NULL) {
   
@@ -30,7 +31,7 @@ beta_part <- function(m, abundance=TRUE, pairwise=FALSE, index_family='sorensen'
   
   if (FD) {
     trait_mat <- trait_mat[dimnames(trait_mat)[[1]] %in% dimnames(m_bin)[[2]], ]
-    core_func <- functional.betapart.core(m_bin, traits = trait_mat)
+    core_func <- functional.betapart.core(m_bin, traits = as.matrix(trait_mat))
     beta_func <- unlist(functional.beta.multi(core_func, index.family = index_family))
   }
   
@@ -45,27 +46,50 @@ beta_part <- function(m, abundance=TRUE, pairwise=FALSE, index_family='sorensen'
     if (abundance) {
       beta_abundance_pair <- beta.pair.abund(core_abundance, index.family = index_family_abund)
     }
+    if (FD) {
+      beta_func_pair <- functional.beta.pair(core_func, traits = trait_mat, index.family = index_family)
+    }
+    if (PD) {
+      beta_phylo_pair <- phylo.beta.pair(core_phy, tree = phylo_tree, index.family = index_family)
+    }
   }
-  # Here we can include functional and phylogenetic if needed.
-  
+
   # Combine and return results.
   res <- data.frame(index = rep(index_family, 3),
-                    divtype = c('replacement','nestedness','total'),
+                    diversity = 'taxonomic',
+                    partition = c('replacement','nestedness','total'),
                     abundance = FALSE,
                     beta = beta_presence)
   if (abundance) {
     res_abund <- data.frame(index = rep(index_family_abund, 3),
-                            divtype = c('replacement','nestedness','total'),
+                            diversity = 'taxonomic',
+                            partition = c('replacement','nestedness','total'),
                             abundance = TRUE,
                             beta = beta_abundance)
     res <- rbind(res, res_abund)
   }
+  if (FD) {
+    res_func <- data.frame(index = rep(index_family, 3),
+                           diversity = 'functional',
+                           partition = c('replacement','nestedness','total'),
+                           abundance = FALSE,
+                           beta = beta_func)
+    res <- rbind(res, res_func)
+  }
+  if (PD) {
+    res_phy <- data.frame(index = rep(index_family, 3),
+                          diversity = 'phylogenetic',
+                          partition = c('replacement','nestedness','total'),
+                          abundance = FALSE,
+                          beta = beta_phy)
+    res <- rbind(res, res_phy)
+  }
   
   if (!pairwise) return(res)
   res <- list(res, beta_presence_pair)
-  if (abundance) {
-    res[[3]] <- beta_abundance_pair
-  }
+  if (abundance) res[[length(res) + 1]] <- beta_abundance_pair
+  if (FD) res[[length(res) + 1]] <- beta_func_pair
+  if (PD) res[[length(res) + 1]] <- beta_phy_pair
   return(res)
 }
 
@@ -75,39 +99,16 @@ beta_part <- function(m, abundance=TRUE, pairwise=FALSE, index_family='sorensen'
 set.seed(444)
 idx_test <- sample(length(all_mats), 10)
 
+# single site test
+beta_part(m = mat_p[1:20,], abundance = TRUE, pairwise = FALSE, index_family = 'sorensen', TD=TRUE, PD=TRUE, FD=FALSE, phylo_tree = pnwphylo)
+
 fia_list <- list()
 
-pb <- txtProgressBar(0, length(idx_test), style=3)
-
-for (p in 1:length(idx_test)) {
-  setTxtProgressBar(pb, p)
-  mat_p <- all_mats[[idx_test[p]]]
-  
-  if(inherits(mat_p, 'matrix')) {
-    if(nrow(mat_p) > 1 & ncol(mat_p) > 1) {
-      
-      # Fix the species names to match the phylogeny, and get rid of the unknown species.
-      
-      mat_p <- mat_p[, dimnames(mat_p)[[2]] %in% pnwphylo$tip.label, drop = FALSE]
-      beta_td <- beta_part(m = mat_p, abundance = TRUE, pairwise = FALSE, index_family = 'sorensen')
-      
-      fia_list[[p]] <- cbind(nneighb = nrow(mat_p) - 1, beta_td)
-      
-    }
-    else {
-      fia_list[[p]] <- cbind(nneighb = NA, null_result)
-    }
-  }
-  else {
-    fia_list[[p]] <- cbind(nneighb = NA, null_result)
-  }
-}
-
-close(pb)
-
-
-##############################################
-# try with other diversity types.
+null_result <- data.frame(index = rep(c('sorensen','bray','sorensen'), each=3),
+                          diversity = rep(c('taxonomic','phylogenetic'), times=c(6,3)),
+                          partition = c('replacement', 'nestedness', 'total'),
+                          abundance = rep(c(FALSE, TRUE, FALSE), each=3),
+                          beta = NA)
 
 pb <- txtProgressBar(0, length(idx_test), style=3)
 
@@ -121,9 +122,9 @@ for (p in 1:length(idx_test)) {
       # Fix the species names to match the phylogeny, and get rid of the unknown species.
       
       mat_p <- mat_p[, dimnames(mat_p)[[2]] %in% pnwphylo$tip.label, drop = FALSE]
-      beta_td <- beta_part(m = mat_p, abundance = TRUE, pairwise = FALSE, index_family = 'sorensen', FD=TRUE, PD=TRUE, phylo_tree=pnwphylo)
-
-      fia_list[[p]] <- cbind(nneighb = nrow(mat_p) - 1, beta_td)
+      beta_p <- beta_part(m = mat_p, abundance = TRUE, pairwise = FALSE, index_family = 'sorensen', TD=TRUE, PD=TRUE, FD=FALSE, phylo_tree = pnwphylo)
+      
+      fia_list[[p]] <- cbind(nneighb = nrow(mat_p) - 1, beta_p)
       
     }
     else {
@@ -136,3 +137,4 @@ for (p in 1:length(idx_test)) {
 }
 
 close(pb)
+
