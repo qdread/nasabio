c4526f397e8be90c696052d355ceb4224ba82467
Quentin Read <quentin.read@gmail.com>
2017-08-31 15:06:09 -0400

    change file path on extract box script

--- data_extraction/extractelevbox_fia.r
+++ data_extraction/extractelevbox_fia.r
@@ -1,6 +1,7 @@
 n_slices <- 5000
 slice <- as.numeric(Sys.getenv('PBS_ARRAYID'))
 raster_file_name <- '/mnt/research/nasabio/data/dem/SRTM_30m_DEM/VRTs/conus_30m_dem.vrt'
+scratch_path <- paste0('/mnt/ffs17/users/', Sys.getenv('USER'))
 
 # FIA lat long coordinates
 library(dplyr)
@@ -31,9 +32,9 @@ for (j in rowidxmin:rowidxmax) {
 	extractBox(coords = with(fiacoords, cbind(lon, lat))[j,,drop=FALSE],
 		   raster_file = raster_file_name,
 		   radius = 300,
-		   fp = '/mnt/research/nasabio/temp',
+		   fp = scratch_path,
 		   filetags = paste('fiatest', row.names(fiacoords)[j], sep = '_'))
-	file_j <- paste0('/mnt/research/nasabio/temp/bbox_fiatest_', row.names(fiacoords)[j], '.tif')
+	file_j <- paste0(scratch_path, '/bbox_fiatest_', row.names(fiacoords)[j], '.tif')
 	stats_by_point[[length(stats_by_point) + 1]] <- statsByRadius(file_j)
 	if (file.exists(file_j)) deleteBox(file_j)
 }	
