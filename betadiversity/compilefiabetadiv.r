7f3f7af9b0682abc326906ac327e80ee63754b25
Quentin Read <quentin.read@gmail.com>
2017-09-08 13:18:33 -0400

    Try to compile very large fia matrices

--- betadiversity/compilefiabetadiv.r
+++ betadiversity/compilefiabetadiv.r
@@ -96,9 +96,9 @@ write.csv(fia_gammadiv, file = '/mnt/research/nasabio/data/fia/fia_gammadiv.csv'
 ###################################
 # 23 Jun: compile FIA beta diversity lookup table
 
-# There are 2000 slices.
+# Update 08 Sep: done again with new traits, now there are 10,000 slices.
 
-n_slices <- 2000
+n_slices <- 10000
 
 fia_betadiv <- list()
 
@@ -113,11 +113,14 @@ for (i in 1:n_slices) {
 close(pb)
 fia_betadiv <- do.call(c, fia_betadiv)
 
+save(fia_betadiv, file = '/mnt/research/nasabio/data/fia/fia_betadivtdpdfd_listbypoint.r')
+
 # Results in a list of pairwise matrices.
 
 # Go through and convert the list into a list of matrices that are essentially pairwise distance matrices.
 # An array can be created for each year. route x route x beta-diversity metric. (or a list of equal size matrices)
 # Then put those arrays into a list.
+### 08 Sep update: Not done because it allocates too much RAM. Instead, let's get the needed values out on a case by case basis.
 
 fia_betadiv_array <- array(NA, dim = c(nrow(fia_betadiv[[1]]), nrow(fia_betadiv[[1]]), ncol(fia_betadiv[[1]])))
 
@@ -127,4 +130,22 @@ for (divmetric in 1:ncol(fia_betadiv[[1]]))
 fia_betadiv_array <- lapply(fia_betadiv_array, function(x) {dimnames(x)[[3]] <- dimnames(fia_betadiv[[1]])[[2]]; x})
 
 save(fia_betadiv_array, file = '/mnt/research/nasabio/data/fia/fia_betadivtdpdfd_array.r')
+
+####
+# Instead of the array, crate pairwise matrices in a list, probably will allocate less memory.
+load('/mnt/research/nasabio/data/fia/fia_betadivtdpdfd_listbypoint.r')
+fia_betadiv_list <- list()
+
+for (divmetric in 1:ncol(fia_betadiv[[1]])) {
+	print(divmetric)
+	list_i <- list()
+	for (plot in 1:length(fia_betadiv)) {
+		list_i[[plot]] <- fia_betadiv[[plot]][, divmetric]
+	}
+	fia_betadiv_list[[i]] <- do.call('cbind', list_i)
+}
+
+names(fia_betadiv_list) <- dimnames(fia_betadiv[[1]])[[2]]
+
+save(fia_betadiv_list, file = '/mnt/research/nasabio/data/fia/fia_betadivtdpdfd_listbymetric.r')
 
