b868ec659451b54b274b241663e63785dcab25b5
Quentin Read <quentin.read@gmail.com>
2017-09-13 17:13:51 -0400

    Added geo stat layers to compilation script

--- data_extraction/compileelev_bioclim.r
+++ data_extraction/compileelev_bioclim.r
@@ -2,6 +2,8 @@
 # BBS and FIA
 # 04 Sep 2017 QDR
 
+# Edited 13 Sep 2017: add geostats (footprint, soil type diversity, and geological age diversity)
+
 ##############################
 # BBS
 
@@ -18,6 +20,9 @@ get_stats <- function(fp, prefix, n, suffix) {
 bbs_elev_stats <- get_stats('/mnt/research/nasabio/data/bbs/elevstats/30m', 'stats_', 1:1000, '.r')
 bbs_bioclim5k_stats <- get_stats('/mnt/research/nasabio/data/bbs/climstats', 'bioclim5k_', 1:500, '.r')
 bbs_bioclim1k_stats <- get_stats('/mnt/research/nasabio/data/bbs/climstats', 'bioclim1k_', 1:1000, '.r')
+bbs_footprint_stats <- get_stats('/mnt/research/nasabio/data/bbs/geostats', 'footprint_', 1:100, '.r')
+bbs_geoage_stats <- get_stats('/mnt/research/nasabio/data/bbs/geostats', 'geoage_', 1:100, '.r')
+bbs_soil_stats <- get_stats('/mnt/research/nasabio/data/bbs/geostats', 'soil_', 1:100, '.r')
 
 # Combine these, dealing with the fact that some list entries are NA.
 
@@ -26,6 +31,14 @@ bbs_5k_missing <- bbs_bioclim5k_stats[[1]]
 bbs_5k_missing[, 3:7] <- NA
 bbs_1k_missing <- bbs_bioclim1k_stats[[1]]
 bbs_1k_missing[, 3:7] <- NA
+bbs_foot_missing <- bbs_footprint_stats[[1]]
+bbs_foot_missing[, 3:7] <- NA
+
+# geological age and soil type diversity have different column labels, so they will need to be joined differently.
+bbs_geo_missing <- bbs_geoage_stats[[1]]
+bbs_geo_missing[, 3:5] <- NA
+bbs_soil_missing <- bbs_soil_stats[[1]]
+bbs_soil_missing[, 3:5] <- NA
 
 bbs_all_stats <- list()
 
@@ -35,7 +48,15 @@ for (i in 1:length(bbs_elev_stats)) {
 	bbs_1k_i <- if (class(bbs_bioclim1k_stats[[i]]) == 'data.frame') bbs_bioclim1k_stats[[i]] else bbs_1k_missing
 	bbs_1k_i$variable <- paste0('bio', 1:19,'_1k')
 	bbs_elev_i <- data.frame(radius = bbs_elev_stats[[i]][,'radius'], variable = 'elevation', bbs_elev_stats[[i]][,c('mean','sd','min','max')], n = NA)
-	bbs_all_stats[[i]] <- cbind(bbsll[i,], rbind(bbs_elev_i, bbs_5k_i, bbs_1k_i))
+	bbs_foot_i <- if(class(bbs_footprint_stats[[i]]) == 'data.frame') bbs_footprint_stats[[i]] else bbs_foot_missing
+	bbs_foot_i$variable <- 'human_footprint'
+	bbs_geo_i <- if(class(bbs_geoage_stats[[i]]) == 'data.frame') bbs_geoage_stats[[i]] else bbs_geo_missing
+	bbs_geo_i$variable <- 'geological_age'
+	bbs_soil_i <- if(class(bbs_soil_stats[[i]]) == 'data.frame') bbs_soil_stats[[i]] else bbs_soil_missing
+	bbs_soil_i$variable <- 'soil_type'
+	
+	bbs_all_stats[[i]] <- full_join(cbind(bbsll[i,], rbind(bbs_elev_i, bbs_5k_i, bbs_1k_i)),
+									cbind(bbsll[i,], rbind(bbs_geo_i, bbs_soil_i)))
 }
 
 bbs_all_stats <- do.call('rbind', bbs_all_stats)
@@ -48,6 +69,10 @@ write.csv(bbs_all_stats, file = '/mnt/research/nasabio/data/bbs/bbs_geodiversity
 fia_elev_stats <- get_stats('/mnt/research/nasabio/data/fia/elevstats/newslice', 'stats_', 1:5000, '.r')
 fia_bioclim5k_stats <- get_stats('/mnt/research/nasabio/data/fia/climstats', 'bioclim5k_', 1:1000, '.r')
 fia_bioclim1k_stats <- get_stats('/mnt/research/nasabio/data/fia/climstats', 'bioclim1k_', 1:5000, '.r')
+fia_footprint_stats <- get_stats('/mnt/research/nasabio/data/fia/geostats', 'footprint_', 1:250, '.r')
+fia_geoage_stats <- get_stats('/mnt/research/nasabio/data/fia/geostats', 'geoage_', 1:250, '.r')
+fia_soil_stats <- get_stats('/mnt/research/nasabio/data/fia/geostats', 'soil_', 1:250, '.r')
+
 
 # FIA lat long coordinates
 library(dplyr)
@@ -63,6 +88,14 @@ fia_1k_missing <- fia_bioclim1k_stats[[10001]]
 fia_1k_missing[, 3:7] <- NA
 fia_elev_missing <- fia_elev_stats[[10001]]
 fia_elev_missing[, 3:7] <- NA
+fia_foot_missing <- fia_footprint_stats[[10001]]
+fia_foot_missing[, 3:7] <- NA
+
+# geological age and soil type diversity have different column labels, so they will need to be joined differently.
+fia_geo_missing <- fia_geoage_stats[[10001]]
+fia_geo_missing[, 3:5] <- NA
+fia_soil_missing <- fia_soil_stats[[10001]]
+fia_soil_missing[, 3:5] <- NA
 
 fia_all_stats <- list()
 
@@ -73,7 +106,15 @@ for (i in 1:length(fia_elev_stats)) {
 	fia_1k_i$variable <- paste0('bio', 1:19,'_1k')
 	fia_elev_i <- if(class(fia_elev_stats[[i]]) == 'data.frame') fia_elev_stats[[i]] else fia_elev_missing
 	fia_elev_i$variable <- 'elevation'
-	fia_all_stats[[i]] <- cbind(as.data.frame(fiacoords[i,]), rbind(fia_elev_i, fia_5k_i, fia_1k_i))
+	fia_foot_i <- if(class(fia_footprint_stats[[i]]) == 'data.frame') fia_footprint_stats[[i]] else fia_foot_missing
+	fia_foot_i$variable <- 'human_footprint'
+	fia_geo_i <- if(class(fia_geoage_stats[[i]]) == 'data.frame') fia_geoage_stats[[i]] else fia_geo_missing
+	fia_geo_i$variable <- 'geological_age'
+	fia_soil_i <- if(class(fia_soil_stats[[i]]) == 'data.frame') fia_soil_stats[[i]] else fia_soil_missing
+	fia_soil_i$variable <- 'soil_type'
+	
+	fia_all_stats[[i]] <- full_join(cbind(fiall[i,], rbind(fia_elev_i, fia_5k_i, fia_1k_i)),
+									cbind(fiall[i,], rbind(fia_geo_i, fia_soil_i)))
 }			
 
 fia_all_stats <- do.call('rbind', fia_all_stats)
