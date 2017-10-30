bdbad5843477c6d91edea6f0fedd350f91eb4fa2
Quentin Read <quentin.read@gmail.com>
2017-10-30 16:57:17 -0400

    Improve subsample code and plot some figs

--- preliminary_stats/div_by_elev_gam_subsampling.r
+++ preliminary_stats/div_by_elev_gam_subsampling.r
@@ -3,11 +3,12 @@
 # Procedure in pseudocode:
 # Load a,b,g diversity data and elevation diversity data
 # Throw out all points within 100 km of Mexico and Canada.
-# for radius in 5, 10, 20, 50, 100 {
-#   for iteration in 1:999 {
+# for iteration in 1:999 { 
+#   Do iterative search to get ~20 points from the dataset (since that is as many 100 km radius ones as we can get)
+#   for radius in 5, 10, 20, 50, 100 {
 #     for y_variable in alpha, beta, gamma {
-#       Do iterative search to get ~20 points from the dataset (since that is as many 100 km radius ones as we can get)
-#       Fit GAM to those points
+#       
+#       Fit GAM to those points, using the correct radius x-var and y-var
 #       Save result to some sort of array
 # }}}
 
@@ -63,12 +64,12 @@ fia_aea_noedge <- fia_aea[!fiacoords$is_edge & fiacoords$CN %in% biogeo$PLT_CN]
 fiacoords_noedge <- subset(fiacoords, !is_edge & CN %in% biogeo$PLT_CN)
 
 
-# Calculate pairwise distances.
+# Calculate pairwise distances. (only takes ~1 min with brute force method)
 fia_pnw_dist <- spDists(fia_aea_noedge, longlat = FALSE)
 
 n_iter <- 10
 subsample_size <- 20
-n_max <- 2000
+n_max <- 50
 
 # R2 of gam. array with number of diversity types (a,b,g) by number of radii by number of iterations
 r2_array <- array(NA, dim = c(length(div_names), length(radii), n_iter))
@@ -77,25 +78,27 @@ r2_array <- array(NA, dim = c(length(div_names), length(radii), n_iter))
 xrange <- range(biogeo$elevation_sd, na.rm=TRUE)
 newx <- round(seq(xrange[1], xrange[2], length.out = 20))
 
+# Predicted value array
+pred_val_array <- array(NA, dim = c(length(div_names), length(radii), n_iter, length(newx)))
+
 pb <- txtProgressBar(0, prod(dim(r2_array)), style = 3)
 ii <- 0
 set.seed(517)
 
+for (k in 1:n_iter) {
+  
+  # Subsample using the largest radius (100 km)
+  sample_idx <- SRS_iterative(focal_points = fia_aea_noedge, dist_mat = fia_pnw_dist, radius = 100e3, n = n_max)
+  
+  # If too many are subsampled, take a random subset of the subsample.
+  if (length(sample_idx) > subsample_size) {
+    plots_in_sample <- fiacoords_noedge$CN[sample(sample_idx, subsample_size)]
+  } else {
+    plots_in_sample <- fiacoords_noedge$CN[sample_idx]
+  }
+  
+  for (j in 1:length(radii)) {
 
-for (j in 1:length(radii)) {
-  for (k in 1:n_iter) {
-    
-    
-    sample_idx <- 1:length(fia_aea_noedge)
-    
-    # Subsample.
-    if (radii[j] > 5) {
-      sample_idx <- SRS_iterative(focal_points = fia_aea_noedge, dist_mat = fia_pnw_dist, radius = radii[j] * 1000, n = n_max)
-    }
-    
-    # If the subsample has more than the requisite number of points, take that many.
-    plots_in_sample <- fiacoords_noedge$CN[sample(sample_idx, subsample_size)] 
-    
     dat <- biogeo %>%
       filter(radius == radii[j], PLT_CN %in% plots_in_sample)
     
@@ -103,10 +106,10 @@ for (j in 1:length(radii)) {
       ii <- ii + 1
       setTxtProgressBar(pb, ii)
       # Fit GAM.
-      gam_fit <- gam(formula(paste('elevation_sd', div_names[i], sep = '~')), data = dat)
+      gam_fit <- gam(formula(paste(div_names[i], 'elevation_sd', sep = '~')), data = dat)
       
       # If possible, get predicted y-values for the gam. This can be used to make an interval on the figures.
-      gam_pred <- predict.gam
+      pred_val_array[i, j, k, ] <- predict.gam(object = gam_fit, newdata = data.frame(elevation_sd = newx))
       
       # Save r-squared.
       r2_array[i, j, k] <- summary(gam_fit)$r.sq
@@ -119,10 +122,39 @@ close(pb)
 # Convert array to data frame
 library(reshape2)
 dimnames(r2_array) <- list(div_names, radii, NULL)
+dimnames(pred_val_array) <- list(div_names, radii, NULL, NULL)
 
 r2_df <- melt(r2_array, varnames = c('diversity_type', 'radius', 'iteration'))
+pred_val_df <- melt(pred_val_array, varnames = c('diversity_type', 'radius', 'iteration', 'x'))
+pred_val_df$x <- newx[pred_val_df$x]
 
 # Plot
 library(cowplot)
+# Variation explained
 ggplot(r2_df, aes(x = radius, y = value, group = interaction(radius, diversity_type), fill = diversity_type)) +
   geom_boxplot()
+
+# Predicted value
+ggplot(pred_val_df, aes(x = x, y = value)) +
+  facet_grid(diversity_type ~ radius, scales = 'free') +
+  geom_line(aes(group = iteration))
+
+ggplot(pred_val_df, aes(x = x, y = value)) +
+  facet_grid(diversity_type ~ radius, scales = 'free') +
+  stat_summary(geom = 'pointrange')
+
+# Try to create a confidence ribbon around the predictive interval so that it looks smoother. Can also plot the points on that.
+pred_val_quant <- pred_val_df %>%
+  group_by(diversity_type, radius, x) %>%
+  summarize(pred_y = quantile(value, probs = 0.5),
+            pred_y_min = quantile(value, probs = 0.025),
+            pred_y_max = quantile(value, probs = 0.975)) %>%
+  arrange(diversity_type, radius, x)
+
+ggplot(biogeo) + 
+  facet_grid(~ radius, scales = 'free') +
+  geom_point(aes(x = elevation_sd, y = alpha_richness), alpha = 0.05) +
+  geom_ribbon(data = pred_val_quant %>% filter(diversity_type == 'alpha_richness'), 
+              aes(x = x, ymin = pred_y_min, ymax = pred_y_max), fill = 'blue', alpha = 0.3) +
+  geom_line(data = pred_val_quant %>% filter(diversity_type == 'alpha_richness'), 
+            aes(x = x, y = pred_y), color = 'blue', size = 1.5)
