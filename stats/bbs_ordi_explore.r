4439556d727c8a5d85fb9ff03e2f3ccf48be0686
Quentin Read <quentin.read@gmail.com>
2018-02-23 15:59:46 -0500

    Create sample subsets of bbs and fia data for google drive

--- stats/bbs_ordi_explore.r
+++ stats/bbs_ordi_explore.r
@@ -20,11 +20,13 @@ bbsgeo <- bbsgeo %>%
   mutate_at(disc_vars, as.factor)
 
 # Get rid of all columns except the point values and the 100 km radius values.
+# Also use only the geodiversity variables that have 5k resolution for better comparison.
 bbsbio <- bbsbio %>%
   select(rteNo, lon, lat, lon_aea, lat_aea, contains('point'), contains('_100'))
 
 bbsgeo <- bbsgeo %>%
-  select(rteNo, lat, lon, HUC4, contains('point'), contains('_100_'))
+  select(rteNo, lat, lon, HUC4, contains('point'), matches('5k.*100'))
+
 
 # Ordinations on bbs geodiversity -----------------------------------------
 
@@ -57,6 +59,7 @@ pred_category <- rep('topography', length(pred_names))
 pred_category[grep('dhi', pred_names)] <- 'DHI'
 pred_category[grep('cloud', pred_names)] <- 'clouds'
 pred_category[grep('human|light', pred_names)] <- 'humans'
+pred_category[grep('geo|soil', pred_names)] <- 'geology'
 # Split up bioclim variables into those about precip and those about temp
 # temp 1-11, precip 12-19
 for (i in 1:19) {
@@ -66,7 +69,7 @@ for (i in 1:19) {
 
 pred_domain <- rep('local', length(pred_names))
 pred_domain[grep('mean', pred_names)] <- 'mean'
-pred_domain[grep('sd', pred_names)] <- 'sd'
+pred_domain[grep('sd|diversity|richness', pred_names)] <- 'sd'
 pred_domain[grepl('tri', pred_names) & !grepl('point', pred_names)] <- 'tri'
 pred_domain[grepl('rough', pred_names) & !grepl('point', pred_names)] <- 'roughness'
 
@@ -77,6 +80,10 @@ pred_df <- data.frame(variable = pred_names,
 # Calculate how each axis is weighted on the different categories.
 # each column of rotation is the axis loadings
 # do one through 10
+
+library(reshape2)
+library(ggplot2)
+
 pred_df <- cbind(pred_df, bbsgeopc$rotation[,1:10])
 
 pred_byaxis <- pred_df %>%
@@ -84,3 +91,145 @@ pred_byaxis <- pred_df %>%
   melt(id.vars = c('domain','category'), value.name = 'loading') %>%
   group_by(variable, domain, category) %>%
   summarize(loading = sum(abs(loading)))
+
+ggplot(pred_byaxis, aes(x = variable, y = loading)) +
+  geom_col(aes(fill = category), position = 'stack') +
+  theme_classic()
+
+ggplot(pred_byaxis, aes(x = variable, y = loading)) +
+  geom_col(aes(fill = domain), position = 'stack') +
+  theme_classic()
+
+# Try to look at the top few variables for each predictor.
+
+pred_melt <- melt(pred_df, id.vars = c('variable','domain','category'), variable.name = 'axis', value.name = 'loading')
+
+get_top <- function(x, ntop = 3) {
+  x[order(abs(x$loading), decreasing = TRUE), ][1:ntop, c('variable','domain','category', 'loading')]
+}
+
+pred_top <- pred_melt %>%
+  group_by(axis) %>%
+  do(get_top(., ntop = 5))
+
+# We can get (sort of) axis interpretations from this:
+# 1 Heterogeneity of climate in region (temperature and cloud cover)
+# 2 A mixed bag climate axis
+# 3 Local and mean temperature
+# 4 Cloud cover and precip locally
+# 5 Local heterogeneity of temperature
+# 6 Local heterogeneity of precip
+# 7 Human impacts
+# 8 Local topography
+# 9 Heterogeneity of climate in region
+# 10 local heterogeneity of cloud cover
+
+# together these ten axes explain over 70% of the variation in "geodiversity".
+# they are really slanted toward climate
+
+# Use geodiversity to predict biodiversity --------------------------------
+
+# Options: use reduced things to predict, or use other techniques like lasso with shrinkage.
+
+# Lasso
+library(glmnet)
+
+# Predict alpha (local), beta over 100 km, and gamma over 100 km
+bbs3bio <- bbsbio %>%
+  select(rteNo, lon, lat, alpha_richness_point, beta_td_sorensen_pa_100, gamma_richness_100) %>%
+  rename(alpha = alpha_richness_point, beta = beta_td_sorensen_pa_100, gamma = gamma_richness_100)
+
+# Aspect has a lot of missing values so get rid.
+
+bbsalpha_dat <- left_join(bbs3bio[,c('rteNo','alpha')], bbsgeo[,-(2:4)]) %>% 
+  select(-contains('mode'), -contains('aspect')) %>%
+  filter(complete.cases(.))
+
+# Select training set
+n <- nrow(bbsalpha_dat)
+set.seed(2002)
+train_idx <- sample(n, n/2, replace = FALSE)
+
+lambdas <- 10 ^ seq(10, -2, length.out = 100)
+
+dat_x <- as.matrix(bbsalpha_dat[train_idx, -(1:2)])
+dat_y <- bbsalpha_dat$alpha[train_idx]
+test_y <- bbsalpha_dat$alpha[-train_idx]
+lasso_mod <- glmnet(x = dat_x, y = dat_y, alpha = 1, lambda = lambdas)
+
+set.seed(2003)
+cv_out <- cv.glmnet(x = dat_x, y = dat_y, alpha = 1)
+
+# Best lambda is chosen to be near zero
+lasso_pred <- predict(lasso_mod, s = cv_out$lambda.min, newx = as.matrix(bbsalpha_dat[-train_idx, -(1:2)]))
+plot(test_y ~ lasso_pred)
+sqrt(mean((lasso_pred - test_y)^2)) #RMSE, it's off by 14.
+abline(0,1,col='red')
+
+lasso_coef <- predict(lasso_mod, type = 'coefficients', s = cv_out$lambda.min) # 97 of the 248 coefficients are not zero.
+
+### 
+# Prediction of beta diversity
+
+bbsbeta_dat <- left_join(bbs3bio[,c('rteNo','beta')], bbsgeo[,-(2:4)]) %>% 
+  select(-contains('mode'), -contains('aspect')) %>%
+  filter(complete.cases(.))
+
+# Select training set
+n <- nrow(bbsbeta_dat)
+set.seed(5005)
+train_idx <- sample(n, ceiling(n/2), replace = FALSE)
+
+lambdas <- 10 ^ seq(10, -2, length.out = 100)
+
+dat_x <- as.matrix(bbsbeta_dat[train_idx, -(1:2)])
+dat_y <- qlogis(bbsbeta_dat$beta[train_idx])
+test_y <- qlogis(bbsbeta_dat$beta[-train_idx])
+lasso_mod <- glmnet(x = dat_x, y = dat_y, alpha = 1, lambda = lambdas)
+
+set.seed(5006)
+cv_out <- cv.glmnet(x = dat_x, y = dat_y, alpha = 1)
+
+# Best lambda is chosen to be near zero
+lasso_pred <- predict(lasso_mod, s = cv_out$lambda.min, newx = as.matrix(bbsbeta_dat[-train_idx, -(1:2)]))
+plot(plogis(test_y) ~ plogis(lasso_pred)) # Back transformation
+sqrt(mean((plogis(lasso_pred) - plogis(test_y))^2)) # RMSE
+abline(0,1,col='red') 
+
+lasso_coef <- predict(lasso_mod, type = 'coefficients', s = cv_out$lambda.min) # 38 of the 248 coefficients are not zero.
+
+
+###
+# predict gamma
+bbsgamma_dat <- left_join(bbs3bio[,c('rteNo','gamma')], bbsgeo[,-(2:4)]) %>% 
+  select(-contains('mode'), -contains('aspect')) %>%
+  filter(complete.cases(.))
+
+# Select training set
+n <- nrow(bbsgamma_dat)
+set.seed(707)
+train_idx <- sample(n, n/2, replace = FALSE)
+
+lambdas <- 10 ^ seq(10, -2, length.out = 100)
+
+dat_x <- as.matrix(bbsgamma_dat[train_idx, -(1:2)])
+dat_y <- bbsgamma_dat$gamma[train_idx]
+test_y <- bbsgamma_dat$gamma[-train_idx]
+lasso_mod <- glmnet(x = dat_x, y = dat_y, alpha = 1, lambda = lambdas)
+
+set.seed(708)
+cv_out <- cv.glmnet(x = dat_x, y = dat_y, alpha = 1)
+
+# Best lambda is a bit bigger.
+lasso_pred <- predict(lasso_mod, s = cv_out$lambda.min, newx = as.matrix(bbsalpha_dat[-train_idx, -(1:2)]))
+plot(test_y ~ lasso_pred)
+sqrt(mean((lasso_pred - test_y)^2)) #RMSE, it's off by 14.
+abline(0,1,col='red')
+
+lasso_coef <- predict(lasso_mod, type = 'coefficients', s = cv_out$lambda.min) # 132 of the 248 coefficients are not zero.
+
+
+
+####
+# Tenfold cross validation.
+
---
+++ stats/pull_sample_data.r
@@ -0,0 +1,111 @@
+# Use stratified sampling to generate a subset of the BBS data.
+# Also ensure that those routes have all data.
+
+r <- 100e3 # minimum distance in m to separate all plots.
+
+source('stats/spatial_fns.r')
+source('stats/SRS_iterative.r')
+
+fp <- 'C:/Users/Q/Dropbox/projects/nasabiodiv'
+
+bbsbioall <- read.csv(file.path(fp, 'bbs_allbio_wide.csv'), stringsAsFactors = FALSE)
+bbsgeoall <- read.csv(file.path(fp, 'bbs_allgeo_wide.csv'), stringsAsFactors = FALSE)
+
+# Find route names and coordinates that have no missing data
+# Nothing is in MNTDfunc so have to get rid
+bbsbioall <- bbsbioall[,-grep('MNTDfunc', names(bbsbioall))]
+
+bbsrtes <- bbsbioall[complete.cases(bbsbioall), 1:5]
+
+library(sp)
+
+# Create spatial info and throw out the plots too close to other countries.
+aea_crs <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0'
+bbssp <- SpatialPoints(coords = bbsrtes[,c('lon_aea','lat_aea')], proj4string = CRS(aea_crs))
+
+# Throw out the ones that are within 100 km of Mexico or Canada (about 200-300)
+bbs_border100 <- flag_coast_plots(focal_points = bbssp, radius = r, border_countries = c('Canada', 'Mexico'))
+
+bbsrtes <- cbind(bbsrtes, bbs_border100)
+bbs_noedge <- bbssp[!bbsrtes$is_edge]
+bbsrtes_noedge <- subset(bbsrtes, !is_edge)
+
+set.seed(1001)
+
+sample_idx <- SRS_iterative_N1(focal_points = bbs_noedge, radius = r, n = 200) # Returns 153 points.
+
+rteids <- bbsrtes_noedge$rteNo[sample_idx]
+
+# Put the biodiversity and geodiversity data from these routes together into a single data frame.
+
+bbsbiosub <- subset(bbsbioall, rteNo %in% rteids)
+bbsgeosub <- subset(bbsgeoall, rteNo %in% rteids)
+
+bbssub <- left_join(bbsbiosub, bbsgeosub)
+
+write.csv(bbssub, file = 'C:/Users/Q/google_drive/NASABiodiversityWG/SampleData/bbs_subset_wideform.csv', row.names = FALSE)
+
+
+#################
+# Pull FIA (run on remote)
+
+r <- 100e3 # minimum distance in m to separate all plots.
+
+source('/mnt/research/nasabio/code/spatial_fns.r')
+source('/mnt/research/nasabio/code/SRS_iterative.r')
+
+fp <- '/mnt/research/nasabio/data/fia'
+
+fiabioall <- read.csv(file.path(fp, 'fia_allbio_wide.csv'), stringsAsFactors = FALSE)
+
+# Load geodiv later when needed.
+
+# Find route names and coordinates that have no missing data
+
+fiaplots <- fiabioall[complete.cases(fiabioall), 'PLT_CN']
+
+source('/mnt/research/nasabio/code/loadfiaall.r')
+
+library(sp)
+
+goodfiacoords <- fiacoords[match(fiaplots, fiacoords$PLT_CN),]
+
+# Create spatial info and throw out the plots too close to other countries.
+aea_crs <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0'
+fia_aea <- spTransform(SpatialPoints(coords=goodfiacoords[,c('lon','lat')], proj4string = CRS('+proj=longlat')), CRSobj = CRS(aea_crs))
+
+# Throw out the ones that are within 100 km of Mexico or Canada (about 200-300)
+fia_border100 <- flag_coast_plots(focal_points = fia_aea, radius = r, border_countries = c('Canada', 'Mexico'))
+
+goodfiacoords <- cbind(goodfiacoords, fia_border100)
+fia_noedge <- fia_aea[!goodfiacoords$is_edge]
+goodfiacoords_noedge <- subset(goodfiacoords, !is_edge)
+
+set.seed(1002)
+
+sample_idx <- SRS_iterative_N1(focal_points = fia_noedge, radius = r, n = 200)  # Returns 142
+
+pltcns <- goodfiacoords_noedge$PLT_CN[sample_idx]
+
+# Put the biodiversity and geodiversity data from these routes together into a single data frame.
+
+fiabiosub <- subset(fiabioall, PLT_CN %in% pltcns)
+
+# Sequentially load each geo data frame, take a subset, and add to what we have.
+
+geo_list <- list()
+
+fiageopt <- read.csv(file.path(fp, 'fia_geo_by_point.csv'), stringsAsFactors = FALSE)
+geo_list[[1]] <- subset(fiageopt, PLT_CN %in% pltcns)
+rm(fiageopt)
+
+for (i in dir(file.path(fp, 'geodiv'), pattern = 'wide')) {
+  print(i)
+  x <- read.csv(file.path(fp, 'geodiv', i))
+  geo_list[[length(geo_list) + 1]] <- subset(x, PLT_CN %in% pltcns)
+  rm(x)
+}
+
+fiasub <- Reduce(left_join, geo_list)
+fiasub <- left_join(fiabiosub, fiasub)
+write.csv(fiasub, file = '/mnt/research/nasabio/data/fia/fia_subset_wideform.csv', row.names = FALSE)
