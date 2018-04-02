bc0b1778cdce76b0ff543e6e7b7786341e22d4e4
Quentin Read <quentin.read@gmail.com>
2018-04-02 07:42:14 -0400

    reduce memory use of script putting together beta diversity metrics

--- run_compile_diversity/fiawholeusa/fia_beta_compilebymetric.r
+++ run_compile_diversity/fiawholeusa/fia_beta_compilebymetric.r
@@ -1,4 +1,12 @@
 # Generate a single matrix 135174 x 135174 for each of the metrics.
+# Edited 30 March: need to store in a less memory intensive way.
+
+# Function to take a vector and return a two column matrix
+# First column is index, second column is value
+col2sparse <- function(x) {
+	notna <- !is.na(x)
+	cbind(which(notna), x[notna])
+}
 
 column <- as.numeric(Sys.getenv('PBS_ARRAYID'))
 
@@ -9,10 +17,13 @@ fp <- '/mnt/research/nasabio/data/fia/diversity/usa'
 
 for (i in 1:n_plot) {
 	load(file.path(fp, paste0('beta_', i, '.r')))
-	metric_list[[i]] <- beta_div[,column]
+	metric_list[[i]] <- col2sparse(beta_div[,column])
 	if(i%%1000==0) print(i)
+	rm(beta_div) # Remove and do garbage collection.
+	gc()
 }
 
-library(dplyr)
-metric_list <- do.call(rbind, metric_list)
-save(metric_list, file = file.path(fp, paste0('matrix_beta_', column, '.r'))
+#library(dplyr)
+#metric_list <- do.call(rbind, metric_list)
+
+save(metric_list, file = file.path(fp, paste0('metric_beta_', column, '.r')))
