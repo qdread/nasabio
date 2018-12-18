e9dbb0174ff2aad239b991f43e238af24c8872a1
Quentin Read <quentin.read@gmail.com>
2018-12-18 14:19:26 -0500

    update script to check what beta jobs still need to run

--- run_compile_diversity/fiawholeusa/betacheck_slurm.r
+++ run_compile_diversity/fiawholeusa/betacheck_slurm.r
@@ -1,16 +1,18 @@
 # Paste together sbatch calls for not completed beta diversity jobs
 
 n <- 119177
+h <- 12
 
 notdone <- which(!file.exists(paste0('/mnt/research/nasabio/data/fia/diversity/usa2018/beta_',1:n,'.r')))
 
 #new_calls <- paste0('sbatch --time=12:00:00 --array=', ifelse(notdone %% 1000 == 0, 1000, notdone %% 1000), ' --export=N1000=', ifelse(notdone %% 1000 == 0, floor(notdone/1000) - 1, floor(notdone/1000)), ' fiabeta.sb')
 
 # Better version to make only one sbatch for each group of 1000.
-thousands <- floor(notdone/1000) - ifelse(notdone%%1000==0, 1, 0)
-new_calls <- sapply(unique(thousands), function(i) {
-	notdone1k <- notdone[thousands == i] %% 1000
-	paste0('sbatch --time=12:00:00 --array=', paste(ifelse(notdone1k == 0, 1000, notdone1k), collapse = ','), ' --export=N1000=', i, ' fiabeta.sb')
+# Better yet break it into groups of 100.
+hundreds <- floor(notdone/100) - ifelse(notdone %% 100==0, 1, 0)
+new_calls <- sapply(unique(hundreds), function(i) {
+	notdone100 <- notdone[hundreds == i] %% 1000
+	paste0('sbatch --time=', h, ':00:00 --array=', paste(ifelse(notdone100 == 0, 1000, notdone100), collapse = ','), ' --export=N1000=', floor(notdone[hundreds == i][1] / 1000), ' fiabeta.sb')
 })
 
 write.table(new_calls, quote = FALSE, row.names = FALSE)
